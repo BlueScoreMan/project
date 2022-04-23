08d443263458c8d50dba40407f19cccc06a90bce
Ufuk Arikan <ufuk.arikaaan@gmail.com>
2022-04-23 14:18:36 +0200

    Add login controller

--- server/src/controllers/v1/index.js
+++ server/src/controllers/v1/index.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import registerRouter from './register';
+import loginRouter from './login';
+
 
 const router = Router();
 
 router.use(registerRouter);
+router.use(loginRouter);
 
 export default router;
